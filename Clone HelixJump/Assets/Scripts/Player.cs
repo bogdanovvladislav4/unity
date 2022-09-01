b0ebaf5ce5eee0850a2fece31b362c5a9bb75166
bogdanovvladislav4 <bogdanovvladislav4@gmail.com>
2022-09-01 16:34:35 +0300

    added shaders

--- Clone HelixJump/Assets/Scripts/Player.cs
+++ Clone HelixJump/Assets/Scripts/Player.cs
@@ -9,6 +9,10 @@ public class Player : MonoBehaviour
     public Rigidbody Rigidbody;
     public Platform CurrentPlatform;
     public Game Game;
+    public Material PlatformDestoed;
+    public Material PlayerDestroed;
+    public AudioClip PlayerDestroedSounds;
+
 
     internal int _platformDestroyedCounter = 0;
     private AudioSource _audio;
@@ -23,6 +27,11 @@ public class Player : MonoBehaviour
         Rigidbody.velocity = new Vector3(0, BounceSpeed, 0);
     }
 
+    public void GameOver()
+    {
+        Game.OnPlayerDied();
+    }
+
     public void ReachFinish()
     {
        Game.OnPlayerReachedFinish();
@@ -31,19 +40,28 @@ public class Player : MonoBehaviour
 
     public void Die()
     {
-        Game.OnPlayerDied();
+        GetComponent<Renderer>().sharedMaterial = PlayerDestroed;
+        _audio.PlayOneShot(PlayerDestroedSounds);
+        Invoke("GameOver", 1f);
         Rigidbody.velocity = Vector3.zero;
     }
 
     private void Update()
     {
         var speed = Rigidbody.velocity.magnitude;
-        if(speed > 30)
+        if(speed > 25)
         {
-            Destroy(CurrentPlatform.gameObject);
+            Transform child = CurrentPlatform.transform.GetChild(0);
+            for(int i= 0; i < child.childCount; i++)
+            {
+                Renderer r = child.GetChild(i).GetComponent<Renderer>();
+                r.sharedMaterial = PlatformDestoed;
+                child.GetChild(i).GetComponent<MeshCollider>().enabled = false;
+            }
+            Destroy(CurrentPlatform.gameObject, 1);
             _audio.Play();
             _platformDestroyedCounter++;
-            Rigidbody.velocity = Vector3.zero;
+            Rigidbody.velocity = new Vector3(0, BounceSpeed - 5, 0);
         }
     }
 }
